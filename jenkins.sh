b2e8cd325627f1a8abfcacb79d6b9d3c4b716e15
sagarmujumale <49682865+sagarmujumale@users.noreply.github.com>
2024-06-26 01:34:57 +0100

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+dnf install docker
+
+systemctl start docker
+
+docker build -t custom-jenkins .
+
+mkdir -p /var/jenkins_home
+chmod 777 /var/jenkins_home
+docker run -d -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home:z --name jenkins custom-jenkins
